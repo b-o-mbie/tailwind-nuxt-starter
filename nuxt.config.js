0d87644d8e4bc7d41349472a6b376b2df11e3fcb
Benedek Halasi <halasibenedek.p@gmail.com>
2021-02-14 15:48:35 +0100

    added default nuxtjs files

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bh-portfolio',
+    title: 'template',
     htmlAttrs: {
       lang: 'en'
     },
